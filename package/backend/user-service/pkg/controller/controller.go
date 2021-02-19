4c7246ed6b9ec3735b9005b3cf29ec3e721217ec
vizitiuRoman <vizitiuroman.md@gmail.com>
2021-02-19 23:40:58 +0200

    Rename ctr to ctrl

--- package/backend/user-service/pkg/controller/controller.go
+++ package/backend/user-service/pkg/controller/controller.go
@@ -20,76 +20,76 @@ func NewUserController(services *services.Manager, logger *zap.SugaredLogger) *U
 	return &UserController{services, logger}
 }
 
-func (ctr *UserController) CreateUser(ctx context.Context, req *pb.CreateUserReq) (*pb.UserRes, error) {
+func (ctrl *UserController) CreateUser(ctx context.Context, req *pb.CreateUserReq) (*pb.UserRes, error) {
 	err := req.Validate()
 	if err != nil {
-		ctr.logger.Errorf("[ctr.CreateUser] error validate user: %v", err)
+		ctrl.logger.Errorf("[ctr.CreateUser] error validate user: %v", err)
 		return nil, status.Errorf(codes.InvalidArgument, err.Error())
 	}
 
-	createdUser, err := ctr.services.User.CreateUser(&User{
+	createdUser, err := ctrl.services.User.CreateUser(&User{
 		Email: req.Email, Password: req.Password,
 	})
 	if err != nil {
-		ctr.logger.Errorf("[ctr.CreateUser] error create user: %v", err)
+		ctrl.logger.Errorf("[ctr.CreateUser] error create user: %v", err)
 		return nil, status.Errorf(codes.AlreadyExists, err.Error())
 	}
 
 	return &pb.UserRes{Id: createdUser.ID, Email: createdUser.Email}, nil
 }
 
-func (ctr *UserController) UpdateUser(ctx context.Context, req *pb.UpdateUserReq) (*pb.UserRes, error) {
+func (ctrl *UserController) UpdateUser(ctx context.Context, req *pb.UpdateUserReq) (*pb.UserRes, error) {
 	err := req.Validate()
 	if err != nil {
-		ctr.logger.Errorf("[ctr.UpdateUser] error validate user: %v", err)
+		ctrl.logger.Errorf("[ctr.UpdateUser] error validate user: %v", err)
 		return nil, status.Errorf(codes.InvalidArgument, err.Error())
 	}
 
-	updatedUser, err := ctr.services.User.UpdateUser(&User{
+	updatedUser, err := ctrl.services.User.UpdateUser(&User{
 		ID: req.Id, Email: req.Email, Password: req.Password,
 	})
 	if err != nil {
-		ctr.logger.Errorf("[ctr.UpdateUser] error update user: %v", err)
+		ctrl.logger.Errorf("[ctr.UpdateUser] error update user: %v", err)
 		return nil, status.Errorf(codes.Internal, err.Error())
 	}
 
 	return &pb.UserRes{Id: updatedUser.ID, Email: updatedUser.Email}, nil
 }
 
-func (ctr *UserController) DeleteUser(ctx context.Context, req *pb.UserReq) (*pb.Stub, error) {
-	err := ctr.services.User.DeleteUser(req.Id)
+func (ctrl *UserController) DeleteUser(ctx context.Context, req *pb.UserReq) (*pb.Stub, error) {
+	err := ctrl.services.User.DeleteUser(req.Id)
 	if err != nil {
-		ctr.logger.Errorf("[ctr.DeleteUser] error delete by id: %v", err)
+		ctrl.logger.Errorf("[ctr.DeleteUser] error delete by id: %v", err)
 		return nil, status.Errorf(codes.Internal, err.Error())
 	}
 	return &pb.Stub{}, nil
 }
 
-func (ctr *UserController) VerifyUser(ctx context.Context, req *pb.VerifyUserReq) (*pb.UserRes, error) {
+func (ctrl *UserController) VerifyUser(ctx context.Context, req *pb.VerifyUserReq) (*pb.UserRes, error) {
 	err := req.Validate()
 	if err != nil {
-		ctr.logger.Errorf("[ctr.VerifyUser] error validate user: %v", err)
+		ctrl.logger.Errorf("[ctr.VerifyUser] error validate user: %v", err)
 		return nil, status.Errorf(codes.InvalidArgument, err.Error())
 	}
 
-	verifiedUser, err := ctr.services.User.VerifyUser(req.Email, req.Password)
+	verifiedUser, err := ctrl.services.User.VerifyUser(req.Email, req.Password)
 	if err != nil {
-		ctr.logger.Errorf("[ctr.VerifyUser] error verify password: %v", err)
+		ctrl.logger.Errorf("[ctr.VerifyUser] error verify password: %v", err)
 		return nil, status.Errorf(codes.InvalidArgument, "Invalid email or password")
 	}
 
 	return &pb.UserRes{Id: verifiedUser.ID, Email: verifiedUser.Email}, nil
 }
 
-func (ctr *UserController) GetUser(ctx context.Context, req *pb.UserReq) (*pb.UserRes, error) {
-	foundUser, err := ctr.services.User.GetUser(req.Id)
+func (ctrl *UserController) GetUser(ctx context.Context, req *pb.UserReq) (*pb.UserRes, error) {
+	foundUser, err := ctrl.services.User.GetUser(req.Id)
 	if err != nil {
-		ctr.logger.Errorf("[ctr.GetUser] error find user by id: %v", err)
+		ctrl.logger.Errorf("[ctr.GetUser] error find user by id: %v", err)
 		return nil, status.Errorf(codes.NotFound, "Not found user")
 	}
 	return &pb.UserRes{Id: foundUser.ID, Email: foundUser.Email}, nil
 }
 
-func (ctr *UserController) GetUsers(ctx context.Context, stub *pb.Stub) (*pb.UsersRes, error) {
+func (ctrl *UserController) GetUsers(ctx context.Context, stub *pb.Stub) (*pb.UsersRes, error) {
 	return &pb.UsersRes{}, nil
 }
